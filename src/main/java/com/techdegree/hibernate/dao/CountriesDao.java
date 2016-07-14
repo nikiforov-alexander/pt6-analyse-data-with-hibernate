bba68da02939cb2f32040d8fa4f1e70d21f85282
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-14 16:48:42 +0200

    added `update` method

--- src/main/java/com/techdegree/hibernate/dao/CountriesDao.java
+++ src/main/java/com/techdegree/hibernate/dao/CountriesDao.java
@@ -8,4 +8,5 @@ public interface CountriesDao {
     public List<Country> findAll();
     public String add(Country country);
     public Country findCountryByCode(String code);
+    public void update(Country country);
 }
