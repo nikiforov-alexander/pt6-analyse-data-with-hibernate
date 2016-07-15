1bb3815275009b9f63fa2572dac21c4c1ca706e3
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-15 12:33:42 +0200

    initial add of test class with proper working`setUp` and `tearDown`. Two first working tests checking addition of new country are added

---
+++ src/test/java/com/techdegree/hibernate/controller/DatabaseMenuPrompterTest.java
@@ -0,0 +1,142 @@
+package com.techdegree.hibernate.controller;
+
+import com.techdegree.hibernate.dao.CountriesDaoImplementation;
+import com.techdegree.hibernate.model.Country;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+import java.io.BufferedReader;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class DatabaseMenuPrompterTest {
+    // test configuration file
+    private static final String TEST_CONFIGURATION_FILE = "hibernate-test.cfg.xml";
+    // static session factory initialized in @BeforeClass
+    private static SessionFactory mSessionFactory;
+    // Dao with all CRUDs we test
+    private CountriesDaoImplementation mCountriesDaoImplementation;
+    // Test country with ABC code
+    private Country mTestCountryWithAbcCode = new Country.CountryBuilder("ABC")
+            .withName("Country")
+            .withAdultLiteracyRate(1.0)
+            .withInternetUsers(1.0)
+            .build();
+    // buffered reader to be mocked and passed to menu prompter, to control
+    // user input
+    private BufferedReader mMockedBufferedReader;
+    // Logger to be mocked and passed to menu prompter to control user
+    // output
+    private Logger mMockedLogger;
+    // main database menu prompter object, created in setUp with DAO
+    private DatabaseMenuPrompter mDatabaseMenuPrompter;
+
+    // adding some decoration before each test
+    @Rule
+    public TestRule watcher = new TestWatcher() {
+        protected void starting(Description description) {
+            System.out.printf("%n --------- Test: %s %n",
+                    description.getMethodName());
+        }
+    };
+
+    @Before
+    public void setUp() throws Exception {
+        // setting up session factory
+        final ServiceRegistry serviceRegistry =
+                new StandardServiceRegistryBuilder()
+                        .configure(TEST_CONFIGURATION_FILE)
+                        .build();
+        mSessionFactory =
+                new MetadataSources(serviceRegistry)
+                        .buildMetadata()
+                        .buildSessionFactory();
+        // setting up DAO with our session factory
+        mCountriesDaoImplementation = new
+                CountriesDaoImplementation(mSessionFactory);
+        // create mocked BufferedReader
+        mMockedBufferedReader = mock(BufferedReader.class);
+        // create mocked Logger
+        mMockedLogger = mock(Logger.class);
+        // creating DatabaseMenuPrompter with DAO, mocked Logger and
+        // mocked BufferedReader to control user input and output
+        mDatabaseMenuPrompter =
+                new DatabaseMenuPrompter(
+                        mMockedBufferedReader,
+                        mMockedLogger,
+                        mCountriesDaoImplementation);
+        //
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mSessionFactory.close();
+    }
+
+    private void addTestCountryToDatabase() {
+        mTestCountryWithAbcCode = new Country.CountryBuilder("ABC")
+                .withName("Country")
+                .withAdultLiteracyRate(null)
+                .withInternetUsers(1.0)
+                .build();
+        mCountriesDaoImplementation.add(mTestCountryWithAbcCode);
+    }
+
+    @Test
+    public void addingNewCountryThroughPrompterWorks() throws Exception {
+        // Given menu with DAO, pointing to empty test database
+        // When user is asked to add new Country and all valid fields
+        // are entered
+        when(mMockedBufferedReader.readLine())
+                .thenReturn("1")
+                .thenReturn("BDC")
+                .thenReturn("Country")
+                .thenReturn("1.0")
+                .thenReturn("null")
+                .thenReturn("0");
+        Country country = new Country.CountryBuilder("BDC")
+                .withName("Country")
+                .withInternetUsers(1.0)
+                .withAdultLiteracyRate(null)
+                .build();
+        // actual menu call
+        mDatabaseMenuPrompter.presentMenuWithPossibleOptions();
+        // Then new country should be in database
+        Country countryFromDataBase =
+                mCountriesDaoImplementation.findCountryByCode("BDC");
+        assertEquals(country, countryFromDataBase);
+    }
+    @Test
+    public void addingNewCountryWithThreeErrorsInvokesErrorsFiveTimes()
+            throws Exception {
+        // Given menu with DAO, pointing to empty test database
+        // When user is asked to add new Country and makes one mistake on
+        // every prompt,
+        when(mMockedBufferedReader.readLine())
+                .thenReturn("a")
+                .thenReturn("1")
+                .thenReturn("adsfdss")
+                .thenReturn("BDC")
+                .thenReturn("1234")
+                .thenReturn("Country")
+                .thenReturn("123.143333333333333333")
+                .thenReturn("1.0")
+                .thenReturn("zero")
+                .thenReturn("null")
+                .thenReturn("0");
+        // actual menu call
+        mDatabaseMenuPrompter.presentMenuWithPossibleOptions();
+        // Then Logger with error message should be invoked 5 times
+        verify(mMockedLogger, times(5)).setErrorMessage(anyString());
+    }
+}
