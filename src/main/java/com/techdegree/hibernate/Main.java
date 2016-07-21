f22a8f814a188f12deb1a405969915b85c625ec0
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-07-21 21:27:35 +0200

    refactored dao name, optimized imports

--- src/main/java/com/techdegree/hibernate/Main.java
+++ src/main/java/com/techdegree/hibernate/Main.java
@@ -3,26 +3,22 @@ package com.techdegree.hibernate;
 import com.techdegree.hibernate.controller.DatabaseMenuPrompter;
 import com.techdegree.hibernate.dao.CountriesDao;
 import com.techdegree.hibernate.dao.CountriesDaoImplementation;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.service.ServiceRegistry;
 
 import java.io.IOException;
 
 public class Main {
     //  create DAO object with all save, find and view methods and pass
     // created session factory there
-    protected static CountriesDao mCountriesDaoImplementation =
+    protected static CountriesDao countriesDao =
             new CountriesDaoImplementation("hibernate.cfg.xml");
 
     public static void main(String[] args) throws IOException {
         // create our main menu object, and pass DAO with database there
         DatabaseMenuPrompter databaseMenuPrompter =
-                new DatabaseMenuPrompter(mCountriesDaoImplementation);
+                new DatabaseMenuPrompter(countriesDao);
         // run the menu
         databaseMenuPrompter.presentMenuWithPossibleOptions();
         // close session factory
-        mCountriesDaoImplementation.close();
+        countriesDao.close();
     }
 }
